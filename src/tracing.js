b62292382b2e18bba20d46a08c95831bbff9150a
chandu_code75 <chandugadidesi@gmail.com>
2026-06-07 22:21:17 +0530

    Added FARO for frontend observability

--- src/tracing.js
+++ src/tracing.js
@@ -8,7 +8,7 @@ import { initializeFaro, getWebInstrumentations } from "@grafana/faro-web-sdk";
 import { TracingInstrumentation } from "@grafana/faro-web-tracing";
 
 initializeFaro({
-  url: "https://faro-collector-prod-ap-south-1.grafana.net/collect/507dc396572937a73cde42db766b8a94",
+  url: "https://faro-collector-prod-ap-south-1.grafana.net/collect/9683f4ab209747c7d9af9051ae276cd2",
   app: {
     name: "done-todoapp-frontend",
     version: "1.0.0",
